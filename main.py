1906419406e222e8926b08eb74bd1620c737ca0d
MagWeb27 <miguelmartinezgomez27@gmail.com>
2026-02-19 17:18:23 -0500

    Nueva funcionalidad, editar titulo de tarea

--- main.py
+++ main.py
@@ -44,11 +44,11 @@ def mostrar_tareas(tasks):
     if not tasks:
         print("\nNo hay tareas registradas.")
     else:
-        print(f"\n{'ID':<5} {'TITULO':<30} {'ESTADO':<12}")
+        print(f"\n{'ID':<5} {'TITULO':<50} {'ESTADO':<12}")
         print("-" * 50)
         for tarea in tasks:
             estado = "Completada" if tarea["completed"] else "Pendiente"
-            print(f"{tarea['id']:<5} {tarea['title']:<30} {estado:<12}")
+            print(f"{tarea['id']:<5} {tarea['title']:<50} {estado:<12}")
     print()
 
 def actualizar_tarea(tasks):
@@ -59,6 +59,20 @@ def actualizar_tarea(tasks):
                 tarea["completed"] = not tarea["completed"]
                 guardar_datos(tasks)
                 print("Estado actualizado correctamente.")
+                return
+        print("La tarea no se encontró.")
+    except ValueError:
+        print("Error: Debes ingresar un número entero.")
+
+def editar_tarea(tasks):
+    try:
+        id_ingresado = int(input("Ingresa el ID de la tarea que deseas editar el nombre: "))
+        for tarea in tasks:
+            if tarea["id"] == id_ingresado:
+                nuevo_titulo = input("Ingresa el nuevo nombre de la tarea: ")
+                tarea["title"] = nuevo_titulo
+                guardar_datos(tasks)
+                print("Nombre actualizado correctamente.")
                 return
         print("La tarea no se encontró.")
     except ValueError:
@@ -87,7 +101,8 @@ while True:
     print("2. Listar tareas")
     print("3. Cambiar estado (Check/Uncheck)")
     print("4. Eliminar tarea")
-    print("5. Salir")
+    print("5. Editar tarea")
+    print("6. Salir")
     
     opcion = input("Selecciona una opción: ")
     
@@ -101,6 +116,8 @@ while True:
         case "4":
             eliminar_tarea(tasks)
         case "5":
+            editar_tarea(tasks)
+        case "6":
             print("¡Hasta luego!")
             break
         case _:
